cdb15da963ec6a4a1c2fe47810df5cada227fa55
William Doane <wdoane@ida.org>
2019-04-01 22:45:19 -0400

    compute right-most #

--- R/comment.R
+++ R/comment.R
@@ -1,38 +1,41 @@
 #' Align Comments
-#'
+#'   
 #' @param filename not implemented yet
-#'
+#'   
 #' @return
 #' @export
-#'
+#'   
 #' @examples
-
+     
 align_comments <-
   function(filename = NULL, ...) {
     if (length(filename) > 1) return(invisible(lapply(filename, align_comments, ...)))
-    
+     
+    doc_id <- rstudioapi::getSourceEditorContext()$id
+     
     lines <- rstudioapi::getSourceEditorContext()$contents
-
+     
     full_line_comments <- stringr::str_detect(lines, "^[ \t]*#")
     locs <- stringr::str_locate(lines, "# ?[^\"']+$")[, 1]
-
+     
     # find farthest right comment mark
     # account for documents containing no lines with trailing comments
-    m <- min(0, max(locs, na.rm = TRUE), na.rm = TRUE)
-
+    m <- max(0, max(locs, na.rm = TRUE), na.rm = TRUE)
+     
     locs[is.na(locs)] <- nchar(lines[is.na(locs)]) + 1
     locs[full_line_comments] <- nchar(lines[full_line_comments]) + 1
-
+     
     code <- stringr::str_trim(stringr::str_sub(lines, 1, locs - 1), side = "right")
     comment <- stringr::str_sub(lines, locs, nchar(lines))
-
+     
     rstudioapi::setDocumentContents(
       paste0(
         stringr::str_pad(code, m, "right"),
         comment,
         collapse = "\n"
       ),
-      rstudioapi::getSourceEditorContext()$id
+      doc_id
     )
-  }
-
+  }  
+     
+     
